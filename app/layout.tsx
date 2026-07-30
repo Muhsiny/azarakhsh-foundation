7c369507eb87ad16cbdf26e972ab095d31dc4806
Muhsiny <138768284+Muhsiny@users.noreply.github.com>
2026-07-30 15:40:04 +0430

    feat: activate universal inline editing across all tabs

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./home-order-fix.css";
 import "./traditional-typography.css";
 import OfflineBootstrap from "./OfflineBootstrap";
 import SiteEnhancer from "./SiteEnhancer";
-import AdminEditShortcut from "./components/AdminEditShortcut";
+import UniversalInlineEditorV2 from "./components/UniversalInlineEditorV2";
 
 const naskh = Noto_Naskh_Arabic({
   subsets: ["arabic"],
@@ -92,7 +92,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <script dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData).replace(/</g, "\\u003c") }} type="application/ld+json" />
         <OfflineBootstrap />
         <SiteEnhancer />
-        <AdminEditShortcut />
+        <UniversalInlineEditorV2 />
         {children}
       </body>
     </html>
