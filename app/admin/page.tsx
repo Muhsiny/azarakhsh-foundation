88a179053de9c2b98c610fc61b3f2a4644f46c5b
Muhsiny <138768284+Muhsiny@users.noreply.github.com>
2026-07-30 14:54:31 +0430

    Enable publication text formatting toolbar

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,7 @@ import AdminDashboard from "./AdminDashboard";
 import LeaderPageManager from "./LeaderPageManager";
 import PageManager from "./PageManager";
 import PublishingHealth from "./PublishingHealth";
+import TextFormattingTools from "./TextFormattingTools";
 
 export const dynamic = "force-dynamic";
 
@@ -27,6 +28,7 @@ export default async function AdminPage() {
 
   return (
     <>
+      <TextFormattingTools />
       <AdminDashboard displayName={user.displayName} signOutHref="/api/auth/logout" />
       <PublishingHealth />
       <LeaderPageManager />
